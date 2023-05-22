356580b6f204b0fa211c63289452c615986b2100
TrosseroAgustin01 <trosseroagustin01@gmail.com>
2023-05-22 09:37:51 -0300

    config db and fix bugs

--- src/Controller/ConferenceController.php
+++ src/Controller/ConferenceController.php
@@ -5,15 +5,21 @@ namespace App\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
-
+use Symfony\Component\HttpFoundation\Request;
 class ConferenceController extends AbstractController
 {
+    /* TODO pasando el valor por query. ej:"https://127.0.0.1:8000/?hello=tronco" */
     #[Route('/', name: 'homepage')]
-    public function index(): Response
+    public function index(Request $request): Response
     {
+        $greet = '';
+        if ($name = $request->query->get('hello')) {
+            $greet = sprintf('<h1>Hello %s!</h1>', htmlspecialchars($name));
+        }
         return new Response(<<<EOF
                 <html>
                     <body>
+                    $greet
                         <img src="/images/under-construction.gif" alt="under-construction">
                     </body>
                 </html>
